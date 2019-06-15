e05cf904a9902d208f4531b1f915e414fd3c80d8
Mosaabk <mosaabk@gmail.com>
2019-06-15 16:29:25 -0500

    First Commit

---
+++ src/main/java/com/waa/dragons/mediationattendance/MediationAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.waa.dragons.mediationattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediationAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediationAttendanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waa/dragons/mediationattendance/MediationAttendanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.waa.dragons.mediationattendance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MediationAttendanceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
