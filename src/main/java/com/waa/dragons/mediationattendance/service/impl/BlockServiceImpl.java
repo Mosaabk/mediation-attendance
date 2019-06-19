c2aa799f90edbefc610ffb30689774352111f42a
Mosaabk <mosaabk@gmail.com>
2019-06-19 15:41:57 -0500

    fix the block service error

--- src/main/java/com/waa/dragons/mediationattendance/service/impl/BlockServiceImpl.java
+++ src/main/java/com/waa/dragons/mediationattendance/service/impl/BlockServiceImpl.java
@@ -28,11 +28,10 @@ public class BlockServiceImpl implements BlockService {
 
     }
 
-
-
-
-
-
+    public String test(){
+        return null;
+    }
+    
     @Override
     public List<Block> findAll() {
         return blockRepository.findAll();
