5b4a75a9cbe32d63b11289f2d88708148c199a69
semharIssac <semharweldegiorgis@gmail.com>
2019-06-20 08:09:56 -0500

    update

--- src/main/java/com/waa/dragons/mediationattendance/controller/StudentController.java
+++ src/main/java/com/waa/dragons/mediationattendance/controller/StudentController.java
@@ -33,39 +33,39 @@ public class StudentController {
     @Autowired
     private BlockService blockService;
 
-    @GetMapping("report")
-    public String getReport(Model model) {
-
-        int totalSessionsPossible = 95;
-        int totalSessionsAttainded = 80;
-        int percentAttainded = totalSessionsAttainded * 100 / totalSessionsPossible;
-        int totalRemaining = (totalSessionsPossible * 60 / 100) - (totalSessionsAttainded);
-
-        model.addAttribute("totalSessionsPossible", totalSessionsPossible);
-        model.addAttribute("totalSessionsAttainded", totalSessionsAttainded);
-        model.addAttribute("percentAttainded", percentAttainded);
-        model.addAttribute("totalRemaining", totalRemaining);
-
-
-        String currentBlock = "July";
-        int sessionInBlock = 22;
-        int daysPresent = 22;
-        int currentBlockPercentage = daysPresent / sessionInBlock * 100;
-        double bonusMark = ((currentBlockPercentage > 90) ? 1.5 : (currentBlockPercentage > 85 ? 1.0 : (currentBlockPercentage > 80 ? 1.0 : 0.0)));
-
-        model.addAttribute("currentBlock", currentBlock);
-        model.addAttribute("sessionInBlock", sessionInBlock);
-        model.addAttribute("daysPresent", daysPresent);
-        model.addAttribute("currentBlockPercentage", currentBlockPercentage);
-        model.addAttribute("bonusMark", bonusMark);
-
-
-        List<String> operators = Arrays.asList("22-June", "23-June", "24-June", "25-June", "26-June", "27-June");
-        model.addAttribute("operators", operators);
-
-        model.addAttribute("firstName", "Abdu");
-        return "studentReport";
-    }
+//    @GetMapping("report")
+//    public String getReport(Model model) {
+//
+//        int totalSessionsPossible = 95;
+//        int totalSessionsAttainded = 80;
+//        int percentAttainded = totalSessionsAttainded * 100 / totalSessionsPossible;
+//        int totalRemaining = (totalSessionsPossible * 60 / 100) - (totalSessionsAttainded);
+//
+//        model.addAttribute("totalSessionsPossible", totalSessionsPossible);
+//        model.addAttribute("totalSessionsAttainded", totalSessionsAttainded);
+//        model.addAttribute("percentAttainded", percentAttainded);
+//        model.addAttribute("totalRemaining", totalRemaining);
+//
+//
+//        String currentBlock = "July";
+//        int sessionInBlock = 22;
+//        int daysPresent = 22;
+//        int currentBlockPercentage = daysPresent / sessionInBlock * 100;
+//        double bonusMark = ((currentBlockPercentage > 90) ? 1.5 : (currentBlockPercentage > 85 ? 1.0 : (currentBlockPercentage > 80 ? 1.0 : 0.0)));
+//
+//        model.addAttribute("currentBlock", currentBlock);
+//        model.addAttribute("sessionInBlock", sessionInBlock);
+//        model.addAttribute("daysPresent", daysPresent);
+//        model.addAttribute("currentBlockPercentage", currentBlockPercentage);
+//        model.addAttribute("bonusMark", bonusMark);
+//
+//
+//        List<String> operators = Arrays.asList("22-June", "23-June", "24-June", "25-June", "26-June", "27-June");
+//        model.addAttribute("operators", operators);
+//
+//        model.addAttribute("firstName", "Abdu");
+//        return "studentReport";
+//    }
 
 
 
@@ -74,7 +74,7 @@ public class StudentController {
         return "reportDetails";
     }
 
-    @GetMapping("/{studentId}")
+    @GetMapping("/report/{studentId}")
     public String getAttendanceList(@PathVariable("studentId") String studentId, Model model){
         Student student = studentService.findStudentByStudentId(studentId);
         Block block = blockService.findBlockByBlockName("2016-December");
@@ -82,7 +82,7 @@ public class StudentController {
         model.addAttribute("attendances", attendanceService.findAllByStudent(student));
         model.addAttribute("blockAttendances",attendanceService.findAllByStudentAndAndBlock(student,block));
 
-        return "attendanceList";
+        return "studentReport";
     }
 
 }
